78e26e1bcab7e597566b8ecf42fc8ec55c3b0670
Nevio <nevio@subspace.com>
2023-10-30 15:31:07 +0100

    Updated deps

--- nodebuilder/store_wasm.go
+++ nodebuilder/store_wasm.go
@@ -2,7 +2,10 @@
 
 package nodebuilder
 
-import "github.com/celestiaorg/celestia-node/indexeddb"
+import (
+	"github.com/celestiaorg/celestia-node/indexeddb"
+	"github.com/ipfs/go-datastore"
+)
 
 func newDataStore(path string) (datastore.Batching, error) {
 	return &indexeddb.DataStore{}, nil
